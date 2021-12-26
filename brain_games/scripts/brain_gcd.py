1e190a2d433a7730d1062382d68558781cb9ea65
SergeyKapinus <kapinus.serg@gmail.com>
2021-12-26 22:34:41 +0300

    Refactoring brain-gcd game

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
-from brain_games.games.divider import divider
+
+"""The scripts start the game 'Brain-gcd'"""
+
+from brain_games.engine import engine
+from brain_games.games import gcd
 
 
 def main():
-    divider()
+    """
+    Program start.
+    Parameters are missing.
+    Returns: None
+    """
+    engine(gcd)
 
 
 if __name__ == '__main__':
