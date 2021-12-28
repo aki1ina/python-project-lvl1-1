4124db884b61fe21779472b3f6a24afa6ba61528
SergeyKapinus <kapinus.serg@gmail.com>
2021-12-28 22:13:55 +0300

    Refactoring brain-prime game

--- brain_games/games/prime_number.py
+++ brain_games/games/prime_number.py
@@ -1,7 +1,6 @@
 """Function of the game 'Brain-game'"""
 
 from random import randint
-from sympy import isprime
 
 GAME_RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
@@ -16,6 +15,29 @@ def get_task_and_solution():
         tuple:(int, str)
     """
     task = randint(1, 20)
-    correct_answer = 'yes' if isprime(task) else 'no'
+    correct_answer = 'yes' if is_prime(task) else 'no'
 
     return task, correct_answer
+
+
+def is_prime(number):
+    """
+    Check the number for simplicity.
+
+    Parameters:
+        number: int
+
+        Returns:
+            bool
+    """
+    if (number < 2):
+        return False
+
+    divider = 2
+
+    while divider <= number / 2:
+        if number % divider == 0:
+            return False
+        divider += 1
+
+    return True
